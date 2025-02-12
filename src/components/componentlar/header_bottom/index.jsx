b80ea870e3eb2867f80abf32bc54a3bb4103f180
muhammad <muhammadnuraliyev119@gmail.com>
2025-02-12 17:05:56 +0500

    texnomart

--- src/components/componentlar/header_bottom/index.jsx
+++ src/components/componentlar/header_bottom/index.jsx
@@ -1,37 +1,29 @@
-import React from "react";
-import Fire02Icon from "../../../assets/fire-02-stroke-rounded";
-import EnergyEllipseIcon from "../../../assets/energy-ellipse-stroke-rounded";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
-function HeaderBottom() {
-  const takliflar = [
-    "Havo sovutgichlar",
-    "Isitgichlar",
-    "Smartfonlar",
-    "Muzlatgichlar",
-    "Changyutgichlar",
-    "Noutbuklar",
-    "Televizorlar",
-    "Barcha kategoriyalar",
-  ];
+function TopCategory() {
+  const [topCategory, setTopCategory] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://gw.texnomart.uz/api/web/v1/header/top-categories")
+      .then((res) => {
+        setTopCategory(res.data.data.data);
+      });
+  }, []);
   return (
-    <div className="container mx-auto mt-6 px-10 flex justify-between">
-      <h4 className="flex gap-2">
-        <Fire02Icon /> Aksiyalar
-      </h4>
-      <h4 className="flex gap-2">
-        <EnergyEllipseIcon /> 1+1
-      </h4>
-      {takliflar.map((item, index) => {
+    <div className="container mx-auto px-10 py-5 flex justify-between">
+      {topCategory.map((item, index) => {
         return (
-          <div key={index}>
-            <p className="font-medium cursor-pointer hover:text-gray-500 transition-all duration-700">
-              {item}
+          <Link key={index} to={`/top-category/${item.slug}`}>
+            <p className="cursor-pointer font-medium hover:text-gray-600">
+              {item.title}
             </p>
-          </div>
+          </Link>
         );
       })}
     </div>
   );
 }
 
-export default HeaderBottom;
+export default TopCategory;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import HeaderTop from "../componentlar/header_top";
 import NavbarTexnomart from "../componentlar/navbar_texnomart";
-import HeaderBottom from "../componentlar/header_bottom";
+import TopCategoriesPage from "../componentlar/header_bottom";
 
 function Navbar() {
   return (
     <div>
       <HeaderTop />
       <NavbarTexnomart />
-      <HeaderBottom />
+      <TopCategoriesPage />
     </div>
   );
 }
