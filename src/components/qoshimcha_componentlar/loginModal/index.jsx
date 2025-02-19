17f8f9218049f2225c0be7f4258720119c8cdd8a
muhammad <muhammadnuraliyev119@gmail.com>
2025-02-19 11:40:23 +0500

    texnomart

--- src/components/qoshimcha_componentlar/loginModal/index.jsx
+++ src/components/qoshimcha_componentlar/loginModal/index.jsx
@@ -1,9 +1,19 @@
 import { UserOutlined } from "@ant-design/icons";
-import { Button, Modal } from "antd";
+import { Button, Input, Modal } from "antd";
+import { useForm } from "react-hook-form";
 import React, { useState } from "react";
 
 function LoginModal() {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <div
@@ -20,13 +30,36 @@ function LoginModal() {
         onCancel={() => {
           setIsOpenModal(false);
         }}
-        onOk={() => {
-          setIsOpenModal(false);
-        }}
+        footer={false}
       >
-        LoginModal
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 25,
+          }}
+        >
+          <h2 className="text-2xl font-medium text-center">
+            Кириш ёки рўйхатдан ўтиш
+          </h2>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-mono">Telefon</label>
+            <input className="boder h-7" {...register("example")} />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-lg font-mono">Ism</label>
+            <input
+              className="boder h-7"
+              {...register("exampleRequired", { required: true })}
+            />
+          </div>
+          <Button htmlType="submit" type="primary" size="large">
+            Kodni olish
+          </Button>
+        </form>
       </Modal>
-    </div>
+    </div>  
   );
 }
 
--- src/components/qoshimcha_componentlar/sideLeft/index.jsx
+++ src/components/qoshimcha_componentlar/sideLeft/index.jsx
@@ -1,4 +1,4 @@
-import { Checkbox, Collapse } from "antd";
+import { Button, Checkbox, Collapse } from "antd";
 import React, { useState } from "react";
 
 function SideLeft({ categories }) {
@@ -15,28 +15,40 @@ function SideLeft({ categories }) {
                 <span className="ml-2 font-mono">{item.count}</span>
               </div>
             ),
-            children:
-              extend === false && item.values.length > 10
-                ? item.values.slice(0, 10).map((i) => {
-                    return (
-                      <div key={i.id}>
-                        <Checkbox>
-                          <span className="font-medium">{i.value}</span>
-                          <span className="font-mono ml-3">{i.count}</span>
-                        </Checkbox>
-                      </div>
-                    );
-                  })
-                : item.values.map((i) => {
-                    return (
-                      <div key={i.id}>
-                        <Checkbox>
-                          <span className="font-medium">{i.value}</span>
-                          <span className="font-mono ml-3">{i.count}</span>
-                        </Checkbox>
-                      </div>
-                    );
-                  }),
+            children: (
+              <div>
+                {extend === false && item.values.length > 10
+                  ? item.values.slice(0, 10).map((i) => {
+                      return (
+                        <div key={i.id}>
+                          <Checkbox>
+                            <span className="font-medium">{i.value}</span>
+                            <span className="font-mono ml-3">{i.count}</span>
+                          </Checkbox>
+                        </div>
+                      );
+                    })
+                  : item.values.map((i) => {
+                      return (
+                        <div key={i.id}>
+                          <Checkbox>
+                            <span className="font-medium">{i.value}</span>
+                            <span className="font-mono ml-3">{i.count}</span>
+                          </Checkbox>
+                        </div>
+                      );
+                    })}
+                <Button
+                  onClick={() => {
+                    setExtend(!extend);
+                  }}
+                  style={{ marginTop: 10 }}
+                  type="primary"
+                >
+                  {extend ? "yopish" : "ko'proq ko'rsatish"}
+                </Button>
+              </div>
+            ),
           };
         })}
       />
